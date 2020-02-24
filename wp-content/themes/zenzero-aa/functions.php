c952cf4df9586e26eb2e9a24f8ff29671b23df67
Sören Boelke <soeren.boelke@gmail.com>
2020-02-25 00:50:21 +0100

    update to last state of php

--- wp-content/themes/zenzero-aa/functions.php
+++ wp-content/themes/zenzero-aa/functions.php
@@ -2,15 +2,13 @@
 
 require get_template_directory() . '/inc/template-tags.php';
 
-define('ZENZERO_AA_VERSION', 'v3');
-
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 add_action( 'wp_enqueue_scripts', 'zenzero_aa_scripts' );
 
 function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', [], ZENZERO_AA_VERSION );
+   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
 function zenzero_aa_scripts() {
-    wp_enqueue_script( 'zenzero-aa-main', get_template_directory_uri() . '/js/jquery.zenzero-aa.js', array(), ZENZERO_AA_VERSION, true );
+    wp_enqueue_script( 'zenzero-aa-main',  get_stylesheet_directory_uri() . '/js/jquery.zenzero-aa.js', ['zenzero-custom'] );
 }
