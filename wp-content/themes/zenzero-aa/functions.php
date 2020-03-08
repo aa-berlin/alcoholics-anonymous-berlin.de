4612e20ff34094e2597e960370b4f51a2a1973bc
Sören Boelke <soeren.boelke@gmail.com>
2020-03-08 01:07:26 +0100

    override builtin pdf generator of TSML plugin class to begin our own list

--- wp-content/themes/zenzero-aa/functions.php
+++ wp-content/themes/zenzero-aa/functions.php
@@ -5,6 +5,8 @@ require get_template_directory() . '/inc/template-tags.php';
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 add_action( 'wp_enqueue_scripts', 'zenzero_aa_scripts' );
 add_action( 'widgets_init', 'zenzero_aa_widgets_init' );
+add_action( 'wp_ajax_tsml_pdf', 'zenzero_aa_tsml_ajax_pdf', 0 );
+add_action( 'wp_ajax_nopriv_tsml_pdf', 'zenzero_aa_tsml_ajax_pdf', 0 );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
@@ -31,4 +33,27 @@ if (function_exists('tsml_custom_types')) {
         'NDA' => 'No Dogs Allowed',
         'IX' => 'Inter',
     ));
+}
+
+// This replaces the builtin meeting-schedule with our own styling.
+// This function should be kept up-to-date with tsml_ajax_pdf().
+function zenzero_aa_tsml_ajax_pdf() {
+
+    //include the file, which includes TCPDF
+    require(TSML_PATH . '/includes/pdf.php');
+    require dirname(__FILE__) . '/inc/BerlinPdf.php';
+
+    //create new PDF document
+    $pdf = new BerlinPdf(array(
+        'margin' => !empty($_GET['margin']) ? floatval($_GET['margin']) : .25,
+        'width' => !empty($_GET['width']) ? floatval($_GET['width']) : 4.25,
+        'height' => !empty($_GET['height']) ? floatval($_GET['height']) : 11,
+    ));
+
+    //send to browser
+    if (!headers_sent()) {
+        $pdf->Output('aa-berlin-meeting-schedule.pdf', 'I');
+    }
+
+    exit;
 }
---
+++ wp-content/themes/zenzero-aa/inc/BerlinPdf.php
@@ -0,0 +1,6 @@
+<?php
+
+class BerlinPdf extends TSMLPDF
+{
+
+}
