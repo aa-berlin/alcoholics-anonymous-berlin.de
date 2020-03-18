f870c11872529ad99bfed99085ba33fbe9ff3cdc
Sören Boelke <soeren.boelke@gmail.com>
2020-03-19 00:31:45 +0100

    add icon to main nav as well

--- wp-content/themes/zenzero-aa/js/jquery.zenzero-aa.js
+++ wp-content/themes/zenzero-aa/js/jquery.zenzero-aa.js
@@ -1,3 +1,7 @@
 ;jQuery(function ($) {
     $('.main-navigation .menu-item-home .fa-home').toggleClass('fa-home fa-coffee');
+
+    const icon = $('<span class="zenzero-aa-stream-icon">').load('/wp-content/themes/zenzero-aa/images/phones.svg');
+
+    $('.main-navigation a[href*="type=ONLINE"]').prepend(icon);
 });
