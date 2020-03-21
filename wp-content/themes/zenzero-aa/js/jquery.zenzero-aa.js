ffbb1e8e9d76e24e56e1f2145292eb2a1233da95
Sören Boelke <soeren.boelke@gmail.com>
2020-03-21 16:16:28 +0100

    add a highlight around a link "/help" in main nav

--- wp-content/themes/zenzero-aa/js/jquery.zenzero-aa.js
+++ wp-content/themes/zenzero-aa/js/jquery.zenzero-aa.js
@@ -1,5 +1,6 @@
 ;jQuery(function ($) {
-    $('.main-navigation .menu-item-home .fa-home').toggleClass('fa-home fa-coffee');
+    $('.main-navigation .menu-item .fa-home').toggleClass('fa-home fa-coffee zenzero-aa-nav-icon');
+    $('.main-navigation .menu-item a[href*="/help"]').prepend('<i class="fa spaceRight fa-question-circle zenzero-aa-nav-icon">').wrapInner('<span class="zenzero-aa-nav-item-background">');
 
     const icon = $('<span class="zenzero-aa-stream-icon">').load('/wp-content/themes/zenzero-aa/images/phones.svg');
 
