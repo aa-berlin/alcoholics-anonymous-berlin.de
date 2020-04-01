15df9613771684bba448c7706a0338ab25adcd79
Sören Boelke <soeren.boelke@gmail.com>
2020-04-01 23:27:10 +0200

    add zoombot plugin with activation dependency of aa-berlin-addons

---
+++ wp-content/plugins/aa-berlin-zoombot/plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin Name: AA Berlin Zoombot
+ * Author: AA Berlin <ecomm.berlin@aamail.org>
+ * Description: Allows for integration with Zoom online meetings providing e.g. coin ceremony features.
+ * Text Domain: aa-berlin-zoombot
+ * Domain Path: /languages/
+ * Version: 0.1.0
+ */
+
+define('AA_BERLIN_ZOOMBOT_VERSION', '0.1.0');
+
+register_activation_hook(__FILE__, 'aa_berlin_zoombot_activate');
+
+function aa_berlin_zoombot_activate() {
+    if (!function_exists('aa_berlin_addons_init')) {
+        deactivate_plugins(plugin_basename(__FILE__ ));
+        wp_die(
+            __('Please install and activate AA Berlin Addons before activating AA Berlin Zoombot.', 'aa-berlin-zoombot'),
+            'Plugin dependency check',
+            array('back_link' => true)
+        );
+    }
+}
