fc3abb36a69daca7a0e475d1a52a9df43fa222b3
Sören Boelke <soeren.boelke@gmail.com>
2020-04-01 22:57:24 +0200

    match examples

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -21,16 +21,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', $_SERVER['WORDPRESS_DB_NAME']);
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', $_SERVER['WORDPRESS_DB_USER']);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', $_SERVER['WORDPRESS_DB_PASSWORD']);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', $_SERVER['WORDPRESS_DB_HOST']);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
