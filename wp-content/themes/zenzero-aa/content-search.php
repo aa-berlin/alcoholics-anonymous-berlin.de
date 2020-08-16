906dd336d5840a959bec755e45d3012fda8c2e6b
Sören Boelke <soeren.boelke@gmail.com>
2020-08-16 19:11:01 +0200

    fix template includes

--- wp-content/themes/zenzero-aa/content-search.php
+++ wp-content/themes/zenzero-aa/content-search.php
@@ -1,7 +1,7 @@
 <?php
 
 ob_start();
-include __DIR__ . '/../zenzero/content.php';
+include __DIR__ . '/../zenzero/content-search.php';
 $parent_content = ob_get_clean();
 
 $parent_content = zenzero_aa_remove_author($parent_content);
--- wp-content/themes/zenzero-aa/content-single.php
+++ wp-content/themes/zenzero-aa/content-single.php
@@ -1,7 +1,7 @@
 <?php
 
 ob_start();
-include __DIR__ . '/../zenzero/content.php';
+include __DIR__ . '/../zenzero/content-single.php';
 $parent_content = ob_get_clean();
 
 $parent_content = zenzero_aa_remove_author($parent_content);
