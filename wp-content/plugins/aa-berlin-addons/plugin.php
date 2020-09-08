d9448b121e8569f3bee3805cad4b4f28a6d79d58
Sören Boelke <soeren.boelke@gmail.com>
2020-09-08 15:45:28 +0200

    new version

--- wp-content/plugins/aa-berlin-addons/plugin.php
+++ wp-content/plugins/aa-berlin-addons/plugin.php
@@ -6,10 +6,10 @@
  * Description: Contains several optimizations and customizations to both WordPress itself and the 12-step-meeting-list plugin.
  * Text Domain: aa-berlin-addons
  * Domain Path: /languages/
- * Version: 1.8.1
+ * Version: 1.9.0
  */
 
-define('AA_BERLIN_ADDONS_VERSION', '1.8.1');
+define('AA_BERLIN_ADDONS_VERSION', '1.9.0');
 
 require __DIR__ . '/includes/options.php';
 require_once ABSPATH . WPINC . '/class-phpass.php';
