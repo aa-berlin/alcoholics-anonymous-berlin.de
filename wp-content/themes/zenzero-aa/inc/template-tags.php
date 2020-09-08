cea0bc41ebe25d72e5b5f0f8330b299e10646c15
Sören Boelke <soeren.boelke@gmail.com>
2020-09-08 15:43:58 +0200

    insert timezone on detail page

--- wp-content/themes/zenzero-aa/inc/template-tags.php
+++ wp-content/themes/zenzero-aa/inc/template-tags.php
@@ -3,3 +3,9 @@
 function zenzero_aa_remove_author($html) {
     return preg_replace('#<span\s+class="byline">.*?</span>.*?</span>#', '', $html);
 }
+
+function zenzero_aa_add_timezone($html) {
+    $timezone = wp_timezone()->getName();
+
+    return preg_replace('#(<p class="meeting-time".+?)(</p>)#s', '$1 <span class="zenzero-aa-timezone"> (' . $timezone .')</span> $2', $html);
+}
---
+++ wp-content/themes/zenzero-aa/single-meetings.php
@@ -0,0 +1,10 @@
+<?php
+
+ob_start();
+include __DIR__ . '/../../plugins/12-step-meeting-list/templates/single-meetings.php';
+$parent_content = ob_get_clean();
+
+$parent_content = zenzero_aa_remove_author($parent_content);
+$parent_content = zenzero_aa_add_timezone($parent_content);
+
+echo $parent_content;
