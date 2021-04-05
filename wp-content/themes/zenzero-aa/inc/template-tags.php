7be5d9d1a56ecd48c5ef678fcf50e42b0cc19396
Sören Boelke <soeren.boelke@gmail.com>
2021-04-05 19:18:24 +0200

    add icons, hover states, move form up

--- wp-content/themes/zenzero-aa/inc/template-tags.php
+++ wp-content/themes/zenzero-aa/inc/template-tags.php
@@ -4,9 +4,17 @@ function zenzero_aa_remove_author($html) {
     return preg_replace('#<span\s+class="byline">.*?</span>.*?</span>#', '', $html);
 }
 
+function zenzero_aa_fix_detail_html($html) {
+    // removes annoying inline style
+    $html = preg_replace('#\bstyle=([\'"]).*?\1#', '', $html);
+    // adds a class for online meeting buttons
+    $html = str_replace('<li class="list-group-item"', '<li class="list-group-item online-meeting-info"', $html);
+    // in this order: email button, back button, email button (placeholder during substitution)
+    $html = str_replace(['glyphicon-chevron-left', 'glyphicon-chevron-right', 'glyphicon-chevron-temp-right'], ['glyphicon-chevron-temp-right', 'glyphicon-chevron-left', 'glyphicon-edit'], $html);
+    // adds icon for location button
+    $html = preg_replace('#(<li.*?list-group-item-location.*?)(</h3>)#s', '$1 <span class="glyphicon glyphicon-chevron-right"></span>$2', $html);
 
-function zenzero_aa_tidy_up_html($html) {
-    return preg_replace('#\bstyle=([\'"]).*?\1#', '', $html);
+    return $html;
 }
 
 function zenzero_aa_add_timezone($html) {
---
+++ wp-content/themes/zenzero-aa/single-locations.php
@@ -0,0 +1,9 @@
+<?php
+
+ob_start();
+include __DIR__ . '/../../plugins/12-step-meeting-list/templates/single-locations.php';
+$parent_content = ob_get_clean();
+
+$parent_content = zenzero_aa_fix_detail_html($parent_content);
+
+echo $parent_content;
