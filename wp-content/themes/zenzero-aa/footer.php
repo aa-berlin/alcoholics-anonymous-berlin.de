a5704f84be6e7f4226fdfac2b115992bfc5650f4
Sören Boelke <soeren.boelke@gmail.com>
2021-04-06 01:25:11 +0200

    move search into header, match odd breakpoint with zenzero

--- wp-content/themes/zenzero-aa/footer.php
+++ wp-content/themes/zenzero-aa/footer.php
@@ -13,6 +13,7 @@ ob_start();
 $service_menu = ob_get_clean();
 
 $parent_footer = preg_replace('#</footer#', "$service_menu $0", $parent_footer);
+$parent_footer = preg_replace('#<div[^>]+open-search[^>]+>.+?</div>#', '', $parent_footer);
 
 // removes global link to parent template, be sure to add back to imprint!
 $parent_footer = preg_replace('#(<div[^>]+site-info[^>]+>.*)<span class="sep">.*(</div>[\r\n\s]*<!--\s*\.site-info\s*-->)#s', '$1 $2', $parent_footer);
--- wp-content/themes/zenzero-aa/header.php
+++ wp-content/themes/zenzero-aa/header.php
@@ -16,10 +16,14 @@ if (current_user_can('read_private_pages')):
 endif;
 $private_menu = ob_get_clean();
 
+// extract inner elements and append private menu
 $private_menu = preg_replace('#^.*?<ul[^>]+>#s', '', $private_menu);
 $private_menu = preg_replace('#</ul></div>.*?$#s', '', $private_menu);
-
 $parent_header = preg_replace('#(site-navigation.*)(</ul>.*?</nav>)#s', "$1 $private_menu $2", $parent_header);
+
 $parent_header = preg_replace('#site-branding.*?</div>#s', "$0 $header_teasers", $parent_header);
+
+$search_button = '<button id="open-search" class="showSearch"><i class="fa fa-search"></i><span class="label">' . translate('Search', 'zenzero-aa') . '</span></button>';
+$parent_header = preg_replace('#<button[^>]+menu-toggle.*?</button>#', "$0 $search_button", $parent_header);
 
 echo $parent_header;
