0f7c73bbee390f9b7ad5abe417265d3ede39b341
Sören Boelke <soeren.boelke@gmail.com>
2021-04-13 16:02:58 +0200

    sidebar as actual sidebar (incomplete, might force-remove)

--- wp-content/themes/zenzero-aa/sidebar.php
+++ wp-content/themes/zenzero-aa/sidebar.php
@@ -4,6 +4,8 @@ ob_start();
 include __DIR__ . '/../zenzero/sidebar.php';
 $parent_content = ob_get_clean();
 
+$parent_content = str_replace('smallPart', '', $parent_content);
+
 echo ZENZERO_AA_SIDEBAR_DELIMITER;
 echo $parent_content;
 echo ZENZERO_AA_SIDEBAR_DELIMITER;
